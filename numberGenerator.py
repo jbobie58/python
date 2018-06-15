05471daefc30e2de016d873dc6423db381dc82e0
joe <joe@joes-MBP.fios-router.home>
2018-06-15 17:05:06 -0400

    Initial commit

---
+++ numberGenerator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+
+def checkNum(maxNum=101):
+    for i in range(maxNum):
+          value = ""
+          if i % 3 == 0: 
+            value += "foo"
+          if i % 5 == 0: 
+            value += "bar"
+          yield value if value else i
+for num, str1 in enumerate(checkNum()):
+    print ("%s: %s" % (num, str1))
---
+++ phoneNumberMatch.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import re, os, sys
+#regular expression that match any strings that are 10-digit phone numbers (e.g. 215-956-8000) 
+
+
+phoneNum = re.compile(r'^(\d{3})-(\d{3})-(\d{4})$')
+print (phoneNum.search('215-956-8000').group())
